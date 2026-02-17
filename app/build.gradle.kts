25d377a0e0610d5cdfe98a907b641282528025bd
Shakil-Flash <tarifulislam492@gmail.com>
2026-02-17 09:18:45 +0600

    new update 4200

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.google.android.material:material:1.13.0")
-    implementation("gson:gson:2.8.6")
+    implementation("com.google.code.gson:gson:2.13.2")
 }
